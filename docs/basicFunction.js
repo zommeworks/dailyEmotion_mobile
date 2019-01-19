e3df45ce54ce8c4ae766c93ad0732ca41bb253b2
myeongsoo.shin <myeongsoo.shin@naverlabs.com>
2019-01-19 21:42:09 +0900

    abandon expand & change to right position control

--- docs/basicFunction.js
+++ docs/basicFunction.js
@@ -50,3 +50,8 @@ function enableScrollMobile(){
   document.removeEventListener('touchmove',preventDefault, false);
 }
 */
+
+function ShuffleArray(o) {
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
