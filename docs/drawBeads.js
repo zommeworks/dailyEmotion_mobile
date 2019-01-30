9dc484bfafc938b3754efd1eecd655bc54765618
zomme <zommeworks@gmail.com>
2019-01-31 00:55:34 +0900

    set multiple animation (not finished)

--- docs/drawBeads.js
+++ docs/drawBeads.js
@@ -400,8 +400,14 @@ function emotionMotion(id){
 	var i = parseInt(beads[parseInt(id)].spectrum);
 	console.log(i);
 	if(i != 0){
-		$("#"+id+" .bead-point-bead").css('animation', 'var(--effect-emotion-'+i+'-bead)');
-		$("#"+id+" .bead-point-shadow").css('animation', 'var(--effect-emotion-'+i+'-shadow)');
+		if(i == 2){
+			$("#"+id+" .bead-point-bead").css('animation', 'var(--effect-emotion-'+i+'-h), var(--effect-emotion-'+i+'-v)');
+			$("#"+id+" .bead-point-shadow").css('animation', 'var(--effect-emotion-'+i+'-h), var(--effect-emotion-'+i+'-shadow)');
+		}
+		else{
+			$("#"+id+" .bead-point-bead").css('animation', 'var(--effect-emotion-'+i+'-bead)');
+			$("#"+id+" .bead-point-shadow").css('animation', 'var(--effect-emotion-'+i+'-shadow)');
+		}
 	}
 }
 function resetEmotionMotion(id){
